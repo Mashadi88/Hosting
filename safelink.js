32fa66583c59b75e771a71588e268eab608ceedc
Mashadi88 <85386844+Mashadi88@users.noreply.github.com>
2022-02-13 06:31:33 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -19,13 +19,11 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*3)+ 1);
 var xxx=null;
-if(x=="1"){xxx="selecting-rules-for-investing-and_18.html"}
-if(x=="2"){xxx="top-10-benefits-of-cloud-hosting-server_18.html"}
-if(x=="3"){xxx="5-benefits-of-using-good-cloud-server.html"}
-if(x=="4"){xxx="5-tips-for-running-successful-home_18.html"}
-if(x=="5"){xxx="how-online-investing-works.html"}a_to_ve[i].href="https://www.imsahadi.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="selecting-rules-for-investing-and.html"}
+if(x=="2"){xxx="top-10-benefits-of-cloud-hosting-server.html"}
+if(x=="3"){xxx="how-online-investing-works.html"}a_to_ve[i].href="https://imsahadiweb.blogspot.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
